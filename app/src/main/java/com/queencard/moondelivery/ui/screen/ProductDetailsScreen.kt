2f0c73929a25b87f22553ad64efb27e8c00d2c4d
kittymoonsz <iamkittymoon@gmail.com>
2024-10-24 20:01:53 -0300

    initial commit

---
+++ app/src/main/java/com/queencard/moondelivery/ui/screen/ProductDetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.queencard.moondelivery.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProductDetailsScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
