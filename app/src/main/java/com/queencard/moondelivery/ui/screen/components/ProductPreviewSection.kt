6b6fe1bcf62958f80c3bccd665887b7aa8ac03c6
kittymoonsz <iamkittymoon@gmail.com>
2024-10-26 16:05:08 -0300

    fixed error and made closeButton

--- app/src/main/java/com/queencard/moondelivery/ui/screen/components/ProductPreviewSection.kt
+++ app/src/main/java/com/queencard/moondelivery/ui/screen/components/ProductPreviewSection.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -17,37 +18,42 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.queencard.moondelivery.R
 
+@Preview
 @Composable
 fun ProductPreviewSection(
     modifier: Modifier = Modifier
 ) {
-
+    Box(modifier = modifier) {
+        Content(
+            modifier = Modifier.statusBarsPadding()
+        )
+    }
 }
 
 @Composable
-//fix it
-//private fun Content(modifier: Modifier = Modifier) {
-//    ConstraintLayout(
-//        modifier = modifier.fillMaxWidth()
-//    ) {
-//        val (actionBar, highlights, productImg) = createRefs()
-//
-//        ActionBar(
-//            headline = "Moon Cheezy",
-//            modifier = Modifier
-//                .padding(horizontal = 18.dp)
-//                .constrainAs(actionBar) {
-//                    top.linkTo(parent.top)
-//                }
-//        )
-//    }
-//}
+private fun Content(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        val (actionBar, highlights, productImg) = createRefs()
+        ActionBar(
+            headline = "Moon Cheezy",
+            modifier = Modifier
+                .padding(horizontal = 18.dp)
+                .constrainAs(actionBar) {
+                    top.linkTo(parent.top)
+                }
+        )
+    }
+}
 
-
+@Composable
 private fun ActionBar(
     modifier: Modifier = Modifier,
     headline: String
@@ -69,11 +75,12 @@ private fun CloseButton(
     modifier: Modifier
 ) {
     Surface(
-        modifier = modifier,
+        modifier = modifier.size(44.dp),
         shape = RoundedCornerShape(16.dp)
     ) {
         Box(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_close),
